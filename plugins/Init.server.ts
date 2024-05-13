e726b64582847020bdb225dae7def8f148df9a55
Hilson <hilsonxhero.me@gmail.com>
2024-05-13 04:02:50 +0330

    Update Init.server.ts

--- plugins/Init.server.ts
+++ plugins/Init.server.ts
@@ -1,12 +1,21 @@
 import { useAuthStore } from "@/modules/auth/store";
 
-export default defineNuxtPlugin((nuxtApp) => {
-    if (process.server) {
-        const store = useAuthStore();
-        return store.init()
-    }
 
+export default defineNuxtPlugin({
+    name: 'my-plugin',
+    enforce: 'post', // or 'post'
+    async setup(nuxtApp) {
+        const store = useAuthStore();
+        await store.init()
+
+    },
+    hooks: {
+        'vue:setup'() {
+            const nuxtApp = useNuxtApp()
+        }
+    },
 
 })
+
 
 
