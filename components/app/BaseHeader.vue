9d0d5afed3c26e96ae5231e0d66b050618204172
Hilson <hilsonxhero.me@gmail.com>
2024-05-22 19:55:26 +0330

    Update BaseHeader.vue

--- components/app/BaseHeader.vue
+++ components/app/BaseHeader.vue
@@ -1,91 +1,5 @@
 <template>
   <div>
-    <!-- <header class="t-header py-4 t-sticky-top">
-      <div class="container">
-        <div class="h-16">
-          <div class="flex items-center h-full justify-between">
-            <div class="flex items-center">
-              <div class="block lg:hidden">
-                <base-button icon variant="gray" @click="showNavHandler">
-                  <nuxt-icon class="text-gray-400" name="menu-bulk"></nuxt-icon>
-                </base-button>
-              </div>
-
-              <nuxt-link to="/" class="hidden lg:block">
-                <div href="" class="t-header__logo">
-                  <img :src="variables?.logo_light" class="" />
-                </div>
-              </nuxt-link>
-
-              <nav
-                ref="navRef"
-                class="t-header__nav lg:bg-transparent flex flex-col w-full lg:w-full z-[6]"
-                :class="{ 'is-active': active }"
-              >
-                <ul
-                  class="flex flex-col space-y-6 lg:space-y-0 p-5 lg:flex-row lg:items-center h-full"
-                >
-                  <li class="ml-5" v-for="(item, index) in menus">
-                    <nuxt-link to="/">
-                      <span class="font-normal text-gray-200">{{
-                        item.title
-                      }}</span>
-                    </nuxt-link>
-                  </li>
-                </ul>
-
-                <div class="flex flex-col pb-6 px-4 md:hidden">
-                  <a
-                    href=""
-                    class="text-white overflow-hidden relative rounded-[14px] py-4 px-8 border border-white border-solid ml-4 block w-full text-center"
-                  >
-                    ورود
-                  </a>
-                  <a
-                    href=""
-                    class="primary-button-two text-white overflow-hidden relative rounded-[14px] py-4 px-8 w-full block mt-2 text-center"
-                  >
-                    شروع رایگان
-                  </a>
-                </div>
-              </nav>
-            </div>
-
-            <div
-              class="flex lg:hidden items-center absolute left-1/2 transform -translate-x-1/2"
-            ></div>
-            <div class="flex items-center">
-              <div class="hidden lg:flex">
-                <template v-if="isLoggedIn">
-                  <nuxt-link
-                    :to="{ name: 'auth' }"
-                    class="text-white overflow-hidden relative rounded-[14px] py-4 px-8 border border-white border-solid ml-4"
-                  >
-                    حساب کاربری
-                  </nuxt-link>
-                </template>
-                <template v-else>
-                  <nuxt-link
-                    :to="{ name: 'auth' }"
-                    class="text-white overflow-hidden relative rounded-[14px] py-4 px-8 border border-white border-solid ml-4"
-                  >
-                    ورود
-                  </nuxt-link>
-                </template>
-
-                <a
-                  href=""
-                  class="primary-button-two text-white overflow-hidden relative rounded-[14px] py-4 px-8"
-                >
-                  شروع رایگان
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header> -->
-
     <div class="section-navbar">
       <div
         data-collapse="medium"
@@ -125,26 +39,26 @@
             </nav>
           </div>
           <div class="nav right">
-            <div class="extra-nav-menu">
+            <div class="hidden lg:block">
               <template v-if="isLoggedIn">
-                <nuxt-link :to="{ name: 'auth' }" class="nav-button w-button">
-                  حساب کاربری
+                <nuxt-link :to="{ name: 'auth' }">
+                  <nuxt-icon class="text-gray-400" name="user-bulk"></nuxt-icon>
                 </nuxt-link>
               </template>
               <template v-else>
-                <nuxt-link :to="{ name: 'auth' }" class="nav-button w-button">
-                  حساب کاربری
+                <nuxt-link :to="{ name: 'auth' }">
+                  <nuxt-icon class="text-gray-400" name="user-bulk"></nuxt-icon>
                 </nuxt-link>
               </template>
             </div>
-            <div class="ml-3">
+            <div class="ml-3 lg:hidden">
               <nuxt-link :to="{ name: 'auth' }">
                 <nuxt-icon class="text-gray-400" name="user-bulk"></nuxt-icon>
               </nuxt-link>
             </div>
 
             <div
-              class="menu-button w-nav-button"
+              class="lg:hidden"
               aria-label="menu"
               role="button"
               tabindex="0"
