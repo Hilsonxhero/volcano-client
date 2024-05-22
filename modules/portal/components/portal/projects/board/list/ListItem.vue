79d71af296e67d363f6fbf8ac711508592824382
Hilson <hilsonxhero.me@gmail.com>
2024-05-22 21:49:47 +0330

    Update ListItem.vue

--- modules/portal/components/portal/projects/board/list/ListItem.vue
+++ modules/portal/components/portal/projects/board/list/ListItem.vue
@@ -53,6 +53,8 @@
             class="relative z-[100] list-group-item group bg-white px-3 py-2 mb-2 handle flex items-center rounded-xl whitespace-normal border-2 hover:border-2 hover:border-blue-600 transition-all ease-linear"
           >
             <div
+              v-on:click.native="handleShowCard(element)"
+              v-on:click="handleShowCard(element)"
               class="hidden absolute top-2 left-3 z-10 group-hover:block group-focus:block transition-all ease-linear delay-200"
             ></div>
             <p class="ellipsis-2">
